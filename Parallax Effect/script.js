95c883a30b7d3936f7d32d813174a27ac0261bae
David Marinovic <marinovic.business@gmail.com>
2022-10-19 16:51:25 +0200

    Animations added

--- Parallax Effect/script.js
+++ Parallax Effect/script.js
@@ -1,17 +1,25 @@
 const wrap = document.querySelector('.wrapper')
 const wrapTwo = document.querySelector('.wrapper-two')
 const footer = document.querySelector('footer')
+const header = document.querySelector('header')
 const lightDarkMode = document.querySelector('.header-buttons img')
 
-const heroTitle = document.querySelector('#hero h2')
 const heroButton = document.querySelector('#hero button')
 const contentHeaderOne = document.querySelector('.content-left-box .one')
 const contentHeaderTwo = document.querySelector('.content-left-box .two')
-const pictureTitle = document.querySelector('.content-right-box h2')
 const contentImage = document.querySelector('.content-right-box img')
+const movingForeground = document.querySelector('#mission .foreground')
 
+const heroTitle = document.querySelector('#hero h2')
+const pictureTitle = document.querySelector('.content-right-box h2')
 const missionTitle = document.querySelector('#mission h2')
 const connectTitle = document.querySelector('#mission h2[title="connect"]')
+
+const contentBg = document.querySelector('#content')
+const navLinks = document.querySelectorAll('ul li a')
+const h2 = document.querySelectorAll('h2')
+const h5 = document.querySelectorAll('h5')
+const p = document.querySelectorAll('p')
 
 // Since my wrappers are not placed ideally, footer would usually stick out until the user scrolls back to the top of the 1st wrapper.
 // To prevent this, I added EL on 2nd wrapper that removes the footer and puts it back again if the bottom is reached.
@@ -19,6 +27,8 @@ let checkPos
 
 wrapTwo.addEventListener('scroll', (e) => {
   let pos = wrapTwo.scrollTop
+  movingForeground.style.left = `${pos}px`
+  movingForeground.style.bottom = `-${pos / 2}px`
   if (pos === 707) {
     checkPos = 550
     footer.classList.remove('hidden')
@@ -31,7 +41,7 @@ wrapTwo.addEventListener('scroll', (e) => {
   if (pos > 150) {
     connectTitle.classList.remove('puff-in')
   }
-  console.log(checkPos, pos)
+  //console.log(checkPos, pos)
 })
 
 // Add text effect depending on the scroll position on the 1st wrapper
@@ -78,6 +88,7 @@ window.addEventListener('load', () => {
 })
 
 // Swap between light and dark mode
+
 let darkMode = false
 
 lightDarkMode.addEventListener('click', () => {
@@ -85,6 +96,7 @@ lightDarkMode.addEventListener('click', () => {
   if (current.includes('dark')) {
     lightDarkMode.setAttribute('src', '/images/lightmode.png')
     darkMode = true
+    setDarkMode()
   }
   if (current.includes('light')) {
     lightDarkMode.setAttribute('src', '/images/darkmode.png')
@@ -98,17 +110,37 @@ lightDarkMode.addEventListener('click', () => {
   }
 })
 
-// Add colors for darkmode in CSS
+// Apply darkmode colors
 function setDarkMode() {
-  console.log(document.querySelector(':root').getAttribute('--navColor'))
+  header.style.backgroundColor = 'var(--navColorDark)'
+  contentBg.style.backgroundColor = 'var(--contentColorDark)'
+  navLinks.forEach(link => link.style.color = 'var(--textColorDark)')
+  h2.forEach(htwo => htwo.style.color = 'var(--textColorDark)')
+  h5.forEach(hfive => hfive.style.color = 'var(--textColorDark)')
+  p.forEach(par => par.style.color = 'var(--textColorDark)')
+  document.querySelector('#logo h4').style.color = 'var(--textColorDark)'
+  document.querySelector('#hero h2').style.color = 'white'
+  document.querySelector('#partners h2').style.color = '#dadada'
+  document.querySelectorAll('#mission .space-left').forEach(side => side.style.backgroundColor = 'var(--contentColorDark)')
+  document.querySelectorAll('#mission .space-right').forEach(side => side.style.backgroundColor = 'var(--contentColorDark)')
 }
 
-// Return original color values in CSS
+//Apply light mode colors
 function setLightMode() {
-  console.log(document.querySelector(':root').getAttributeNames())
-
+  header.style.backgroundColor = 'var(--navColor)'
+  contentBg.style.backgroundColor = 'var(--contentColor)'
+  navLinks.forEach(link => link.style.color = 'var(--mainColor)')
+  h2.forEach(htwo => htwo.style.color = 'var(--textColor)')
+  h5.forEach(hfive => hfive.style.color = 'var(--textColor)')
+  p.forEach(par => par.style.color = 'var(--textColor)')
+  document.querySelector('#logo h4').style.color = 'var(--textColor)'
+  document.querySelector('#hero h2').style.color = 'white'
+  document.querySelector('#partners h2').style.color = '#dadada'
+  document.querySelector('#mission .space-left').style.backgroundColor = '#dadada'
+  document.querySelectorAll('#mission .space-left').forEach(side => side.style.backgroundColor = 'var(--contentColor)')
+  document.querySelectorAll('#mission .space-right').forEach(side => side.style.backgroundColor = 'var(--contentColor)')
 }
-// +++++++ ADD TEXT ANIMATIONS ON SCROLL, ADD MEDIAS +++++
+// +++++++ ADD MEDIAS, GET PERCENTAGE OF CURRENT SCROLL LOCATION AND DISPLAY IT +++++
 
-// header, nav, buttons, #content, content h2, content p
-// header 
+// Place the position at which you want the scroll animations to happen in a variable - var = window.innerHeight, maybe include getBoundingClientRect() for wrappers
+
