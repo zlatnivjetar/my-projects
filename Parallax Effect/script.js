fb568c59f272db40155a1c16770872279c64815f
David Marinovic <marinovic.business@gmail.com>
2022-10-21 23:20:32 +0200

    Code update

--- Parallax Effect/script.js
+++ Parallax Effect/script.js
@@ -5,6 +5,7 @@ const header = document.querySelector('header')
 const lightDarkMode = document.querySelector('.header-buttons img')
 
 const heroButton = document.querySelector('#hero button')
+const contentBg = document.querySelector('#content')
 const contentHeaderOne = document.querySelector('.content-left-box .one')
 const contentHeaderTwo = document.querySelector('.content-left-box .two')
 const contentImage = document.querySelector('.content-right-box img')
@@ -15,77 +16,63 @@ const pictureTitle = document.querySelector('.content-right-box h2')
 const missionTitle = document.querySelector('#mission h2')
 const connectTitle = document.querySelector('#mission h2[title="connect"]')
 
-const contentBg = document.querySelector('#content')
 const navLinks = document.querySelectorAll('ul li a')
 const h2 = document.querySelectorAll('h2')
 const h5 = document.querySelectorAll('h5')
 const p = document.querySelectorAll('p')
 
-
 // Since my wrappers are not placed ideally, footer would usually stick out until the user scrolls back to the top of the 1st wrapper.
 // To prevent this, I added EL on 2nd wrapper that removes the footer and puts it back again if the bottom is reached.
 let checkPos
-
 wrapTwo.addEventListener('scroll', (e) => {
   let pos = wrapTwo.scrollTop
-  let triggerPos = wrapTwo.getBoundingClientRect().height * 1.05
+  let triggerPos = wrapTwo.getBoundingClientRect().height * 1.03
   let connectTop = connectTitle.getBoundingClientRect().top
 
   if (connectTop < triggerPos) {
     connectTitle.classList.remove('puff-in')
   }
 
-  if (pos === 707) {
-    checkPos = 550
+  if (pos > 530) {
+    checkPos = 400
     footer.classList.remove('hidden')
   }
   if (pos < checkPos) {
+    wrapTwo.scrollTo(0, checkPos + 80)
     footer.classList.add('hidden')
     checkPos = null
-    wrapTwo.scrollTo(0, 700)
   }
 
   movingForeground.style.left = `${pos}px`
   movingForeground.style.bottom = `-${pos / 2}px`
-  console.log(connectTop, triggerPos)
+  console.log(pos)
 })
-
-// RECONDITION ALL ANIMATIONS BASED ON EITHER WINDOW INNERHEIGHT OR wrapTwo triggerPos
 
 // Add text effect depending on the scroll position on the 1st wrapper
 wrap.addEventListener('scroll', (e) => {
   let pos = wrap.scrollTop
-  //console.log(pos)
-
-  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   let triggerPos = window.innerHeight * 0.85
-  let headerTop = contentHeaderTwo.getBoundingClientRect().top
-  if (headerTop < triggerPos) {
+  let headerOneTop = contentHeaderOne.getBoundingClientRect().top
+  let headerTwoTop = contentHeaderTwo.getBoundingClientRect().top
+  let pictureTitlePos = pictureTitle.getBoundingClientRect().top
+  let contentImagePos = contentImage.getBoundingClientRect().top
+  let missionTitlePos = missionTitle.getBoundingClientRect().top
+
+  if (headerTwoTop < triggerPos) {
     contentHeaderTwo.classList.remove('from-left')
   }
-  //console.log(triggerPos)
-  //console.log(headerTop)
-  // I MANAGED TO ACTIVATE FX BY CHECKING IF THE TOP OF content h2 IS SMALLER THAN SET-UP WINDOW HEIGHT TRIGGER POSITION
-  // i need to find the same fuctionality for wrapper two, because it changes total height when on smaller screen
-
-  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  if (pos > 150) {
+  if (headerOneTop < triggerPos) {
     contentHeaderOne.classList.remove('from-left')
   }
-  if (pos > 200) {
+  if (pictureTitlePos < triggerPos) {
     pictureTitle.classList.remove('puff-in')
   }
-  if (pos > 250) {
+  if (contentImagePos < triggerPos) {
     contentImage.classList.remove('tilt-in')
   }
-  /*if (pos > 370) {
-    contentHeaderTwo.classList.remove('from-left')
-  }*/
-  if (pos > 680) {
+  if (missionTitlePos < triggerPos) {
     missionTitle.classList.remove('puff-in')
   }
-
-
   if (pos < 50) {
     contentHeaderOne.classList.add('from-left')
     contentHeaderTwo.classList.add('from-left')
@@ -104,8 +91,17 @@ window.addEventListener('load', () => {
   }, 3000);
 })
 
-// Swap between light and dark mode
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const navMobile = document.querySelector('nav.mobile')
 
+// Open mobile navigation
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('open')
+  navMobile.classList.toggle('open')
+  wrap.classList.toggle('open')
+})
+
+// Swap between light and dark mode
 let darkMode = false
 
 lightDarkMode.addEventListener('click', () => {
@@ -161,14 +157,3 @@ function setLightMode() {
   document.querySelectorAll('#mission p').forEach(text => text.style.color = '#000000')
 
 }
-
-const hamburgerMenu = document.querySelector('.hamburger-menu')
-const navMobile = document.querySelector('nav.mobile')
-hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('open')
-  navMobile.classList.toggle('open')
-  wrap.classList.toggle('open')
-})
-// +++++++ ADD MEDIAS, GET PERCENTAGE OF CURRENT SCROLL LOCATION AND DISPLAY IT +++++
-// Place the position at which you want the scroll animations to happen in a variable - var = window.innerHeight, maybe include getBoundingClientRect() for wrappers
-
